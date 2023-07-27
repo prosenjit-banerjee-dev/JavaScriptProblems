9a64317b82eecabf27da040365255c32f8c880e9
PB Rudro <pbrudro@gmail.com>
2023-07-27 18:54:21 +0600

    problem number four

--- JS01.js
+++ JS01.js
@@ -37,3 +37,16 @@ function mostFrequentElement(array) {
   }
   return mostFrequentElement;
 }
+
+// Problem 04
+function findTwoNumbers(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
