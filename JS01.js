3e5bc3d6fba493d21f7ffb17a90c378b4f66fce4
PB Rudro <pbrudro@gmail.com>
2023-07-27 18:39:55 +0600

    three problem solved

---
+++ JS01.js
@@ -0,0 +1,39 @@
+// Problem 01
+function reverseString(input) {
+  let reversed = "";
+  for (let i = input.length - 1; i >= 0; i--) {
+    reversed += input[i];
+  }
+  return reversed;
+}
+const example = "hello world";
+const output = reverseString(example);
+console.log(output);
+
+// Problem 02
+function sumPositiveNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+const inputArray = [2, -5, 10, -3, 7];
+const result = sumPositiveNumbers(inputArray);
+console.log(result);
+
+// Problem 03
+function mostFrequentElement(array) {
+  let mostFrequentElement = array[0];
+  let mostFrequentFrequency = 1;
+
+  for (let element of array) {
+    if (array.includes(element) > mostFrequentFrequency) {
+      mostFrequentElement = element;
+      mostFrequentFrequency = array.includes(element);
+    }
+  }
+  return mostFrequentElement;
+}
